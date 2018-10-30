741862c2b1a7aecc54ee0001c62b24bf7d81cb2b
Luca Gualtieri <lucag@quasargrp.local>
2018-10-29 20:18:22 -0700

    Add project files.

---
+++ Power BI Survey/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Power_BI_Survey.Startup))]
+namespace Power_BI_Survey
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
